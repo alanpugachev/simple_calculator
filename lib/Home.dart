409c328f89320dd7bf8a2ccb7caf5f774125492b
alanpugachev <aszneesizzle@gmail.com>
2022-06-29 17:09:49 +0300

    Add interface

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +10,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget btn(String text, Color btncolor, Color textcolor) {
+    return Container(
+      child: RaisedButton(
+        onPressed: (){
+
+        },
+        child: Text(
+          text, 
+          style: TextStyle(
+            fontSize: 35,
+            color: textcolor,
+          ),
+        ),
+        shape: CircleBorder(),
+        color: btncolor,
+        padding: EdgeInsets.all(20),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +52,68 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                btn('AC', Colors.grey, Colors.black),
+                btn('+/-', Colors.grey, Colors.black),
+                btn('%', Colors.grey, Colors.black),
+                btn('/', Colors.amber, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                btn('7', Colors.blueGrey, Colors.white),
+                btn('8', Colors.blueGrey, Colors.white),
+                btn('9', Colors.blueGrey, Colors.white),
+                btn('*', Colors.amber, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                btn('4', Colors.blueGrey, Colors.white),
+                btn('5', Colors.blueGrey, Colors.white),
+                btn('6', Colors.blueGrey, Colors.white),
+                btn('-', Colors.amber, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                btn('1', Colors.blueGrey, Colors.white),
+                btn('2', Colors.blueGrey, Colors.white),
+                btn('3', Colors.blueGrey, Colors.white),
+                btn('+', Colors.amber, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  padding: EdgeInsets.fromLTRB(34, 20, 128, 20),
+                  onPressed: () { 
+
+                  },
+                  child: Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.white
+                    ),
+                  ),
+                  shape: StadiumBorder(),
+                  color: Colors.blueGrey,
+                ),
+                btn('.', Colors.blueGrey, Colors.white),
+                btn('=', Colors.amber, Colors.white),
               ],
             )
           ],
